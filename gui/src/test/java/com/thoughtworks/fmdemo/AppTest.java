38bcc4eecd7c770f81eb2488eeee47e546da5e1a
Kevin Hickey <khickey@thoughtworks.com>
2015-01-06 22:16:31 -0500

    Made a gui project to have a multi-component application

---
+++ gui/src/test/java/com/thoughtworks/fmdemo/AppTest.java
@@ -0,0 +1,15 @@
+package com.thoughtworks.fmdemo;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void shouldPassBecauseThatIsGoodToo() {
+        assertThat(true, is(true));
+
+    }
+}
